9b0e4a5b2b932a04ab2af705865e62140988e444
cyl628 <yl-chen21@mails.tsinghua.edu.cn>
2022-10-18 16:27:30 +0800

    update trainig script

--- train.sh
+++ train.sh
@@ -3,10 +3,10 @@ echo $data
 
 # use soft-prompt
 prompt='soft'
-# use hard-prompt
+# use hard-prompt (main experiments)
 # prompt='hard3'
 
-# baseline BERT-CLS
+# baseline BERT-CLS Finetuning
 python -u train.py --model baseline --model_name bert-base-cased --data $data --lr 2e-5 --usecls --epoch 500 --batch_size 32 --val_batch_size 64 --val_step 2000 --log_step 1000
 
 # prompt typing 
@@ -14,4 +14,4 @@ python -u train.py --model baseline --model_name bert-base-cased --data $data --
 python -u train.py --model maskedlm --model_name bert-base-cased --data $data --prompt $prompt  --lr 5e-5 --sample_num 1
 
 # 8 shot
-python -u train.py --model maskedlm --model_name bert-base-cased --data $data --prompt $prompt  --lr 5e-5 --sample_num 1
+python -u train.py --model maskedlm --model_name bert-base-cased --data $data --prompt $prompt  --lr 5e-5 --sample_num 8
